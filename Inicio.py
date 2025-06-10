c314c23b889cce5942e00ca7cfe5814ae0a5a915
Juann2408 <juayalaar@unal.edu.co>
2025-06-10 09:38:22 -0500

    *9

--- Inicio.py
+++ Inicio.py
@@ -5,7 +5,7 @@ import random
 Dado = random.randint(1, 6)
 Lados= [n  for n in range (9)]
 for l in (Lados):
-  l=["▭","▭","▭","▭","▭","▭","▭","▭","▭"]
+  l=["▭"]*9
   for i in l:
     i=("▭")
     print(i, end="  ")
