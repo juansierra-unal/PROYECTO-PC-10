65f69e2450f2188d31147c2322052b9965827fe2
Juann2408 <juayalaar@unal.edu.co>
2025-06-09 23:46:56 -0500

    Cambio de tablero

--- Inicio.py
+++ Inicio.py
@@ -3,12 +3,10 @@
 
 import random
 Dado = random.randint(1, 6)
-Gato= [n  for n in range (9)]
-print(Gato)
-
-tablero = [[(i*9) + h for h in range(10)] for i in range (9)]
-for i in tablero:
-    for h in i:
-        print(h, end=" ")
-
-    print()
+Lados= [n  for n in range (9)]
+for l in (Lados):
+  l=["▭","▭","▭","▭","▭","▭","▭","▭","▭"]
+  for i in l:
+    i=("▭")
+    print(i, end="  ")
+  print()
